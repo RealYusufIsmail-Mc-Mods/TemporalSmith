79112597dda5ee3d02db181b28ead9c191246e53
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-26 10:32:44 +0000

    getting ready for ore gen

---
+++ src/main/kotlin/io/github/realyusufismail/armourandtoolsmod/datagen/world/oregen/ModOreFeaturesGen.kt
@@ -0,0 +1,11 @@
+package io.github.realyusufismail.armourandtoolsmod.datagen.world.oregen
+
+import io.github.realyusufismail.realyusufismailcore.data.support.oregen.ModOreFeaturesSupport
+import net.minecraft.data.worldgen.BootstapContext
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature
+
+class ModOreFeaturesGen : ModOreFeaturesSupport() {
+    override fun bootstrap(context: BootstapContext<ConfiguredFeature<*, *>>?) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/io/github/realyusufismail/armourandtoolsmod/datagen/world/oregen/ModOrePlacementsGen.kt
@@ -0,0 +1,11 @@
+package io.github.realyusufismail.armourandtoolsmod.datagen.world.oregen
+
+import io.github.realyusufismail.realyusufismailcore.data.support.oregen.ModOrePlacementsSupport
+import net.minecraft.data.worldgen.BootstapContext
+import net.minecraft.world.level.levelgen.placement.PlacedFeature
+
+class ModOrePlacementsGen : ModOrePlacementsSupport() {
+    override fun bootstrap(context: BootstapContext<PlacedFeature>?) {
+        TODO("Not yet implemented")
+    }
+}
